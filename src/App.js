e727110f3d04cf3bcb4dba231f339cb98c959c8a
AugustasKl <augustas.klima@gmail.com>
2021-09-14 20:28:22 +0300

    header fixed and buttons working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import HomePage from "./pages/homepage/homepage.component";
 import {Switch, Route} from 'react-router-dom'
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 const HatsPage =()=> (
@@ -18,6 +19,7 @@ const HatsPage =()=> (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path ='/' component={HomePage} />
         <Route path ='/shop' component={ShopPage} />
